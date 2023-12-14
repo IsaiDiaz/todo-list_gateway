63dfffc495f28914a7a3014de1ca6b919c54b07b
IsaiDiaz <jsoza057@gmail.com>
2023-12-14 17:45:13 -0400

    base functionality

--- src/main/kotlin/com/todo_list/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/todo_list/gateway/GatewayApplication.kt
@@ -2,8 +2,10 @@ package com.todo_list.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class GatewayApplication
 
 fun main(args: Array<String>) {
